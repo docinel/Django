59f60e2fe9dbb6e036f9be5c63d74ce5da5edc00
docinel <docinel@gmail.com>
2023-09-17 23:53:33 -0300

    Criando template

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from core.models import Evento
 # Create your views here.
+def lista_evendos(request):
+    evento = Evento.objects.get(id=1)
+    response = {'evento': evento}
+    return render(request, 'agenda.html', response)
